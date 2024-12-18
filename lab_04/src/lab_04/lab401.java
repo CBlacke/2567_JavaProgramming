ece4da4555fcca7f393f6d6c4d78f99033032b32
CBlacke <choku3475@gmail.com>
2024-12-18 09:52:58 +0700

    lab04

--- lab_04/src/lab_04/lab401.java
+++ lab_04/src/lab_04/lab401.java
@@ -1,10 +1,36 @@
 package lab_04;
-
+import javax.swing.*;
 public class lab401 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		inputEmail();
+	}
+	
+	public static void inputEmail() {
+		String ans;
+		String varEmail;
+		do {
+			varEmail = JOptionPane.showInputDialog("Input your e-mail: ");
+			if(varEmail != null && !varEmail.isEmpty()) {
+				checkEmailError(varEmail);
+			}else {
+				JOptionPane.showMessageDialog(null, "E-mail cannot be empty!!!");
+			}
+			ans =JOptionPane.showInputDialog("Do you want to input email address[Y/y]");
+		}while(ans !=null && ans.equalsIgnoreCase("y"));
+	}
+	public static void checkEmailError(String email) {
+		while(email.startsWith("@")||email.contains(" ")) {
+			email = JOptionPane.showInputDialog("Invaild e-mail. Input your e-mail, again: ");
 
+		}
+		email =email.toLowerCase();
+		JOptionPane.showMessageDialog(null, checkEmail(email)?"Your e-mail is "+email 
+				:"Your e-mail is not a hotmail or gmail." );
+	}
+	public static boolean checkEmail(String email) {
+		
+		return email.endsWith("@gmail.com")||email.endsWith("hotmail.com");
 	}
 
 }
