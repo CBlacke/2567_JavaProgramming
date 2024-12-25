6cb6eed77f2869461cdede27baf3d56293492c1f
CBlacke <choku3475@gmail.com>
2024-12-25 11:08:47 +0700

    lab05

--- lab_05/src/lab_05/lab507.java
+++ lab_05/src/lab_05/lab507.java
@@ -1,10 +1,16 @@
 package lab_05;
-
+import javax.swing.*;
 public class lab507 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	public static void showEvent(int[] nums) {
+		
+	}
+	public static void showOdd(int[] nums) {
+		
 	}
 
 }
