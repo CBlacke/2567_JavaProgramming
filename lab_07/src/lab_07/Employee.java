da99fb5736accc8c03dde11c527310e4accc699d
CBlacke <choku3475@gmail.com>
2025-01-22 11:08:20 +0700

    lab07

---
+++ lab_07/src/lab_07/Employee.java
@@ -0,0 +1,13 @@
+package lab_07;
+
+public class Employee {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int salary;
+	
+	Employee(){
+		
+	}
+}
---
+++ lab_07/src/lab_07/raiseSalary.java
@@ -0,0 +1,10 @@
+package lab_07;
+
+public class raiseSalary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
